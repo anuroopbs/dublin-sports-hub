3e4ba8068e927e93084cc857d3d1f7ebad34f285
anuroopbs <anuroopquestion7@gmail.com>
2025-03-23 22:41:07 +0000

    Update FindPlayer.js

--- components/FindPlayer.js
+++ components/FindPlayer.js
@@ -1,1 +1,9 @@
+export default function FindPlayer() {
+  return (
+    <div style={{ marginTop: '20px' }}>
+      <h2 style={{ fontSize: '24px' }}>Find Players</h2>
+      <p>This is where players will be listed. Coming soon!</p>
+    </div>
+  );
+}
 
